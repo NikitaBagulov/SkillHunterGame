e22895bad4132b541a0b69125952392c36e589ba
NikitaBagulov <117837048+NikitaBagulov@users.noreply.github.com>
2025-04-25 19:06:50 +0800

    fix resolution and add logo

--- GUI/LoadingScreen/loading_screen.gd
+++ GUI/LoadingScreen/loading_screen.gd
@@ -16,15 +16,16 @@ func set_progress(value: float) -> void:
 	progress_bar.value = value
 	if value >= 100.0 and not is_loading_complete:
 		is_loading_complete = true
-		label.text = "Press ENTER to continue..."  # Меняем текст
+		label.text = "Нажмите ENTER для продолжения..."  # Меняем текст
 		set_process_input(true)  # Включаем обработку ввода для ожидания клавиши
 
 func show_loading() -> void:
 	Inventory.visible = false
+	Hud.visible = false
 	visible = true
 	animation_player.play("fade_in")
 	is_loading_complete = false
-	label.text = "Loading..."
+	label.text = "Загрузка..."
 	progress_bar.value = 0
 	get_tree().set_pause(true)  # Приостанавливаем игру (блокируем ввод и физику)
 	
@@ -33,11 +34,11 @@ func hide_loading() -> void:
 	await animation_player.animation_finished
 	visible = false
 	Inventory.visible = false
+	Hud.visible = true
 	is_loading_complete = false
 	set_process_input(false)  # Отключаем обработку ввода
 	get_tree().set_pause(false)
 
 func _input(event: InputEvent) -> void:
 	if is_loading_complete and event is InputEventKey and event.pressed:
-		#print(event)
 		hide_loading()  # Скрываем экран при нажатии любой клавиши
